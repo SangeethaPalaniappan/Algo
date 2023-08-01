6e7f5d3d2d262e52be46bb0f14f53b4d4d1a9c77
Sangeetha <sangeethapa2002@gmail.com>
2023-08-01 12:45:57 +0530

    Hashing - Insertion,Deletion,Search

--- Hashing.py
+++ Hashing.py
@@ -1,20 +1,41 @@
-size=int(input("size:"))
-a=[-1]*size
-print(a)
 
+#Inserting
 def hashing(val,size):
     s=val%size
+    start=s
     print(s)
-    if a[s]!=-1:
-        while a[s]!=-1:
-            s=s+1
-            if s==size:
-                s=0
-        a[s]=val  
-    else:
-        a[s]=val
-    print(a)
     
-for x in range(size):
-    val=int(input("val:"))
-    hashing(val,size)    
+    while a[s]!=-1:
+        s=s+1
+        if s==size:
+            s=0
+        if s==start:
+            print("Hash table is Full")
+            return a
+    a[s]=val  
+    
+    return a
+
+size=int(input("size:"))
+a=[-1]*size
+print(a)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+val=int(input("val:"))
+a=hashing(val,size)    
+
+ 
