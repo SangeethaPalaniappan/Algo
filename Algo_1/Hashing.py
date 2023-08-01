102ebeea13125ce812bd773f7810c4fd332010e9
Sangeetha <sangeethapa2002@gmail.com>
2023-08-01 13:00:00 +0530

    Duplicate file Deleted

--- Algo_1/Hashing.py
+++ Algo_1/Hashing.py
@@ -1,7 +1,6 @@
-
 #Inserting
 def hashing(val,size):
-    s=val%size
+    s=val%size #position to be added [size divides the value and the value stored in the reminder] 
     start=s
     print(s)
     
@@ -15,6 +14,37 @@ def hashing(val,size):
     a[s]=val  
     
     return a
+
+#Deleting
+def delhashing(val,size):
+    d=val%size
+    f=d
+    while a[f]!=val:
+        f+=1
+        if f==size:
+            f=0
+        if f==d:
+            print("No element found")
+            return a
+    a[f]=-1   #deleting the element by giving the value -1 to the element
+    return a
+    
+
+
+#Searching
+def search_hashing(val,size):
+    u=val%size
+    m=u
+    while a[m]!=val:
+        m+=1
+        if m==size:
+            m=0
+        if m==u:
+            print("Searched Element not found")
+            return a
+    else:
+        print("Element found")
+        return a
 
 size=int(input("size:"))
 a=[-1]*size
@@ -30,12 +60,15 @@ val=int(input("val:"))
 a=hashing(val,size)    
 
 val=int(input("val:"))
-a=hashing(val,size)    
+a=search_hashing(val,size)    
 
 val=int(input("val:"))
-a=hashing(val,size)    
+a=search_hashing(val,size)    
 
 val=int(input("val:"))
-a=hashing(val,size)    
+a=delhashing(val,size)    
+
+val=int(input("val:"))
+a=delhashing(val,size)    
 
  
